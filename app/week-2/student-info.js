64a830e5bfa82fd34717401f10c90f9d39cea34f
Gerazel Castro <gerazelcastro@gmail.com>
2025-05-13 20:11:36 -0600

    Update student-info.js

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,7 +4,7 @@ export default function Studentinfo() {
     return (
         <div>
         <h1>Gerazel Jane Castro</h1>
-        <Link href="https://github.com/AselC"className="hover:underline hover:text-cyan-300"></Link>
+        <Link href="https://github.com/AselC/cprg306-assignments"className="hover:underline hover:text-cyan-300">https://github.com/AselC/cprg306-assignments</Link>
         </div>
     );
 }
